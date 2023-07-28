7c208ae2aef32bcab4bfbdf9ab2067df9d1b1168
Swapnil Mundaware <swapnilmundaware98@gmail.com>
2023-07-29 00:06:33 +0530

    listing Paged added with Listing Search

--- src/pages/productLisiting.js
+++ src/pages/productLisiting.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import NavBar from "../components/NavBar";
 
 const url =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=KHkDvlCSDPyNwIu82ycr9x4twfu0fdl0T9V_CbuujNMhPrHMoWiQHQRFw2LZIL9N9nxN3AxUh4s_QGFd7Duc5sfKSlsQrzjbm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLY999MAnJKWd_Ogd1xf_4-nA7D4VdPXzSfsudt4iYRcepzAUjZpKq5lSHuyOISmynFmgTuarEu2BA6sgBa34SJ7qroWavlf0g&lib=MePB_WRytVvz23dr296_xE9QXSiohcWGh";
+  "https://script.googleusercontent.com/macros/echo?user_content_key=7Unh1kAyAOZ6F8noo69HbStgYELO7SuGQoojhP2w35LWUHtWLRRLi16WTy4NkT-RF_p-s2tQZ5j7vudGmk5NzfV82RDSxuXym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLj4JCqQhfZE9tDCyL0DsaDvKfWJJkz_bHpsIQJQVpvOA7ljR2sCK6FuWDVWN13hQBmSiiBxXJBxolW0w1Xz8msRJb3Q5XrcdA&lib=MePB_WRytVvz23dr296_xE9QXSiohcWGh";
 
 const ProductListing = () => {
   const [properties, setProperties] = useState([]);
@@ -14,6 +14,7 @@ const ProductListing = () => {
       .then((response) => response.json())
       .then((data) => {
         setProperties(data.data);
+        console.log(data.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -53,6 +54,10 @@ const ProductListing = () => {
             <p>City: {property.city}</p>
             <p>State: {property.state}</p>
             <p>Country: {property.country}</p>
+            <p>Agent Name : {property.agent}</p>
+            <button>
+              <a href={`mailto:${property.email}`}>Contact Now</a>
+            </button>
             <div>
               <img src={property.Pic1} alt={`Image 1`} />
               <img src={property.Pic2} alt={`Image 2`} />
