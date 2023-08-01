bf1a9b431ee521de83c0f7791cb594f50e835f54
Swapnil Mundaware <93301234+SwapzzzGit@users.noreply.github.com>
2023-08-01 18:38:03 +0530

    Update productLisiting.js

--- src/pages/productLisiting.js
+++ src/pages/productLisiting.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Dropdown from "../components/dropdown";
-import NavBar from "../components/NavBar";
 import Pagination from "../components/pagination";
+import NavBar from "../components/NavBar";
 
 const url =
   "https://script.googleusercontent.com/macros/echo?user_content_key=7Unh1kAyAOZ6F8noo69HbStgYELO7SuGQoojhP2w35LWUHtWLRRLi16WTy4NkT-RF_p-s2tQZ5j7vudGmk5NzfV82RDSxuXym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLj4JCqQhfZE9tDCyL0DsaDvKfWJJkz_bHpsIQJQVpvOA7ljR2sCK6FuWDVWN13hQBmSiiBxXJBxolW0w1Xz8msRJb3Q5XrcdA&lib=MePB_WRytVvz23dr296_xE9QXSiohcWGh";
@@ -11,6 +11,7 @@ const ProductListing = () => {
   const [properties, setProperties] = useState([]);
   const [search, setSearch] = useState("");
   const [filteredProperties, setFilteredProperties] = useState([]); // 1. #1 State Variable for filtering the property search
+
   const fetchPropertiesData = () => {
     fetch(url)
       .then((response) => response.json())
@@ -41,6 +42,7 @@ const ProductListing = () => {
     });
     setFilteredProperties(searchedProperties);
   };
+  console.log(filteredProperties);
   // 2. #1 Seach even after hitting "Enter key"
   const handleEnterKey = (e) => {
     if (e.key === "Enter") {
@@ -61,8 +63,10 @@ const ProductListing = () => {
       );
     })
     .slice(0, 5);
+
   //console.log(propertySuggestions);
   return (
+    //Seach Input +Dropdown + Search Button
     <div>
       <div>
         <input
@@ -81,29 +85,7 @@ const ProductListing = () => {
         <button onClick={handleSearchButtonClick}>Search</button>
         {/*1. #3 Button for filtered the property search*/}
       </div>
-
-      {filteredProperties.length > 0 &&
-        filteredProperties.map((property) => (
-          <Card key={property.id}>
-            <img src={property.homeMainPic} alt="Property" />
-            <p>Street Address: {property.address}</p>
-            <p>Price: ${property.price}</p>
-            <p>City: {property.city}</p>
-            <p>State: {property.state}</p>
-            <p>Country: {property.country}</p>
-            <p>Agent Name : {property.agent}</p>
-            <button>
-              <a href={`mailto:${property.email}`}>Contact Now</a>
-            </button>
-            <div>
-              <img src={property.Pic1} alt={`Image 1`} />
-              <img src={property.Pic2} alt={`Image 2`} />
-              <img src={property.Pic3} alt={`Image 3`} />
-              <img src={property.Pic4} alt={`Image 4`} />
-              <img src={property.Pic5} alt={`Image 5`} />
-            </div>
-          </Card>
-        ))}
+      <Pagination filteredProperties={filteredProperties} />
     </div>
   );
 };
