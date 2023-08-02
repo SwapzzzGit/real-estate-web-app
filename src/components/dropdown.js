49fc4b96da1119fc6bc8440f06ec9c0eba6738e3
Swapnil Mundaware <93301234+SwapzzzGit@users.noreply.github.com>
2023-08-02 12:53:26 +0530

    Update dropdown.js

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -2,17 +2,27 @@ import React from "react";
 
 const Dropdown = ({ suggestions, handleSuggestionClick }) => {
   console.log(suggestions);
+
+  const uniqueStates = new Set(
+    suggestions.map((item) => `${item.city.toLowerCase()}`)
+  );
+
+  const stateOptions = Array.from(uniqueStates).slice(0, 6);
+
   return (
     <datalist id="suggestions">
-      {suggestions.map((suggestion) => (
-        <option
-          key={suggestion.id}
-          value={suggestion.city}
-          onClick={() => handleSuggestionClick(suggestion.city)}
-        >
-          {suggestion.city}
-        </option>
-      ))}
+      {stateOptions.map((suggestion) => {
+        console.log(suggestion); // Add this line to log each suggestion
+        return (
+          <option
+            key={suggestion.id}
+            value={suggestion.city}
+            onClick={() => handleSuggestionClick(suggestion.city)}
+          >
+            {suggestion.city}
+          </option>
+        );
+      })}
     </datalist>
   );
 };
